c707e8d7c17de9825a76a69ddcb445524e5f4d4a
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-01 21:14:19 +0500

    Create index.js

---
+++ Intentional Error/index.js
@@ -0,0 +1,9 @@
+let dinnerInvitationList = ['Saleem', 'Yasir', 'Khurram'];
+
+for (let i = 0; i < dinnerInvitationList.length; i++) {
+    console.log(dinnerInvitationList[i] + ' you are invited for dinner.');
+}
+
+console.log(dinnerInvitationList[3]); //result will be undefined because index 3 of array is not assigned 
+
+console.log(dinnerInvitationList[2]); // now this will print index 2
