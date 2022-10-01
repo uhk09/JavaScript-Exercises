681c067a070ef50609a5471ecacdf9a3040be71c
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-01 21:05:28 +0500

    Create index.js

---
+++ Excerc ise Number 20/index.js
@@ -0,0 +1,9 @@
+let cultureOfCountries = [
+    ["Indus", "Chenaab", "Satlaj"],
+    ['Karachi', 'Lahore', 'Islamabad'],
+    ['Urdu', 'Punjabi', 'Pushtoo']
+];
+
+for (let i = 0; i < cultureOfCountries.length; i++) {
+    console.log(cultureOfCountries[i]);
+}
