c8ccd46c0eefba007c63e90b0823ce93f7f1b73e
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-01 11:27:09 +0500

    Create index.js

---
+++ YourOwn Array/index.js
@@ -0,0 +1,8 @@
+let myCars = ['Fortuner', 'Sportage', 'V8', 'Civic', 'BMW'];
+
+for (let i = 0; i < myCars.length; i++) {
+    console.log(myCars[i] + ' is my favuorite car and I want to own this.');
+    console.log(myCars[i] + ' is luxurious car.');
+    console.log(' Soon I will buy ' + myCars[i]);
+
+}
