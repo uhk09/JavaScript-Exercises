59258169ed28b7cd09a236b1db8b04758c3fb42e
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-01 20:48:55 +0500

    Create index.js

---
+++ Seeing the World/index.js
@@ -0,0 +1,16 @@
+let arr = ["karachi", "hyderabad", "sukkhar", "queta", "peshawar"];
+console.log(arr);
+
+let arrSort = arr.slice();
+console.log(arrSort.sort());
+console.log(arr);
+
+console.log(arrSort.reverse());
+console.log(arr);
+
+console.log(arr.reverse());
+console.log(arr.reverse());
+
+console.log(
+    arr.sort());
+console.log(arr.reverse());
