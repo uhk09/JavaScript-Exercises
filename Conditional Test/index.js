b88cca335340b9e6bf5a0a0f1d4478ed9e82c06b
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-01 21:45:06 +0500

    Create index.js

---
+++ Conditional Test/index.js
@@ -0,0 +1,7 @@
+let month = 'October';
+let car = 'Suzuki';
+let year = 2022;
+
+console.log(month == "October" ? "I predicted Correct Month" : 'Wrrong Month Prediction');
+console.log(car == "Toyota" ? "I predicted Correct Car" : 'Wrrong Car Prediction');;
+console.log(month == "October" ? "I predicted Correct Year" : 'Wrrong Year Prediction');;
