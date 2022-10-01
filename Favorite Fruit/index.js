c7dafaa5f20941fb93245cafc69b08af0a8da7d4
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-01 23:56:48 +0500

    Create index.js

---
+++ Favorite Fruit/index.js
@@ -0,0 +1,26 @@
+let favorite_fruits = ['Red Apple', 'Mango', 'Banana'];
+
+if (favorite_fruits.indexOf('Red Apple') !== -1) {
+    console.log("It's your favourite Red Apple");
+
+}
+
+if (favorite_fruits.indexOf('Mango') !== -1) {
+    console.log("Wow! It's Mango");
+
+}
+
+if (favorite_fruits.indexOf('Banana') !== -1) {
+    console.log("Yummy Banana");
+
+}
+
+if (favorite_fruits.indexOf('Pinapple') !== -1) {
+    console.log("Big pineapple");
+
+}
+
+if (favorite_fruits.indexOf('watermelon') !== -1) {
+    console.log("Watermelon");
+
+}
