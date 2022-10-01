fbcc47ad971a9570cba3846eebe3ed160d5de442
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-01 22:54:54 +0500

    Create index.js

---
+++ Alien Colors/index-fail.js
@@ -0,0 +1,5 @@
+let alien_color = "green";
+
+if (alien_color === "green") {
+
+}
---
+++ Alien Colors/index-pass.js
@@ -0,0 +1,5 @@
+let alien_color = "green";
+
+if (alien_color === "green") {
+    console.log("Player earned 5 points");
+}
