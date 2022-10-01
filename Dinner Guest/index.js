a3e000da3e29932f6de5a5871f548ec0352b09b4
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-01 20:52:08 +0500

    Create index.js

---
+++ Dinner Guest/index.js
@@ -0,0 +1,7 @@
+let dinnerInvitationList = ['Saleem', 'Yasir', 'Khurram'];
+
+for (let i = 0; i < dinnerInvitationList.length; i++) {
+    console.log(dinnerInvitationList[i] + ' you are invited for dinner.');
+}
+
+console.log(dinnerInvitationList.length);
