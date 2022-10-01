f782383f8ec760ca373e41e317efdb49628f4833
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-01 10:42:01 +0500

    Create index.js

---
+++ Number Eight/index.js
@@ -0,0 +1,18 @@
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let ten = 10;
+let sixteen = 16;
+
+//Addition
+console.log(five + three);
+
+//Subtraction
+console.log(ten - two);
+
+//Multiplication
+console.log(four * two);
+
+//Division
+console.log(sixteen / two);
