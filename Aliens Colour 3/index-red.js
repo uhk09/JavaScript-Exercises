137b881caf387ecb9701a73dfcfb98657792d08e
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-01 23:13:14 +0500

    create index file

---
+++ Aliens Colour 3/index-red.js
@@ -0,0 +1,12 @@
+let alien_color = 'red';
+
+if (alien_color === 'green') {
+    console.log("Player earned 5 points.");
+
+} else if (alien_color === 'yellow') {
+    console.log("Player earned 10 points.");
+
+} else if (alien_color === 'red') {
+    console.log("Player earned 15 points.");
+
+}
