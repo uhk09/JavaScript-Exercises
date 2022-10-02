c9352e372c1ecc80c3136981a49f03f9bf61ccfa
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-02 13:00:50 +0500

    Create index.js

---
+++ Large Shirts/index.js
@@ -0,0 +1,19 @@
+let sizeShirt = "16";
+let messageShirt = '"I love coding"';
+
+function make_shirt(sizeShirt) {
+
+    if (sizeShirt === "Large" || sizeShirt === "Medium") {
+        console.log(sizeShirt + ' is the size of shirt' + '\nWhile the required message is ' + " I Love Javascript");
+
+    } else {
+        console.log(sizeShirt + ' is the size of shirt' + '\nWhile the required message is ' + " I Love Pakistan");
+
+    }
+
+
+
+
+}
+
+make_shirt(sizeShirt);
