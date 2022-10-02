1d1831f526efa9cdb11b4e0f5ab5f76c80b936eb
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-02 13:53:41 +0500

    Update index.js

--- Album/index.js
+++ Album/index.js
@@ -6,10 +6,11 @@ let album1 = 'Album Sajjad';
 let album2 = 'Album Ali';
 let album3 = 'Album Junaid';
 
-function make_album(artistName, albumTitle) {
+function make_album(artistName, albumTitle, numberTracks = 10) {
     return {
         artistName: artistName,
         albumTitle: albumTitle,
+        numberTracks: numberTracks
     }
 
 }
@@ -18,6 +19,8 @@ let album = [];
 
 album.push(make_album(artist1, album1));
 album.push(make_album(artist2, album2));
-album.push(make_album(artist3, album3));
+album.push(make_album(artist3, album3, 15));
 
-console.log(album);
+for (let i = 0; i < album.length; i++) {
+    console.log(album[i]);
+}
