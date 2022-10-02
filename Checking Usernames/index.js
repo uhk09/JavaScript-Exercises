a06660bdf24a452b19e0f42c9626c5ffa086d06d
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-02 12:08:00 +0500

    create index file

---
+++ Checking Usernames/index.js
@@ -0,0 +1,11 @@
+let current_users = ['taha', 'kamran', 'faisal', 'khurram', 'ali'];
+let new_users = ['shahzad', 'saleem', 'ali', 'faisal', 'haseeb'];
+
+for (let i = 0; i < current_users.length; i++) {
+    for (let j = 0; j < current_users.length; j++) {
+        if () {
+
+        }
+
+    }
+}
---
+++ Ordinal Numbers/index.js
@@ -0,0 +1,25 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i = 0; i < 10; i++) {
+    if (i === 1) {
+        console.log(i + 'st');
+
+    } else if (i === 2) {
+        console.log(i + 'nd');
+    } else if (i === 3) {
+        console.log(i + 'rd');
+    } else if (i === 4) {
+        console.log(i + 'th');
+    } else if (i === 5) {
+        console.log(i + 'th');
+    } else if (i === 6) {
+        console.log(i + 'th');
+    } else if (i === 7) {
+        console.log(i + 'th');
+    } else if (i === 8) {
+        console.log(i + 'th');
+    } else if (i === 9) {
+        console.log(i + 'th');
+    }
+
+}
