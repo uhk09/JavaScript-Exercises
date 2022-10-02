fc433f7faf0f01e41137077e12838bdb46448d72
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-02 14:27:50 +0500

    Create index.js

---
+++ Magicians/index.js
@@ -0,0 +1,10 @@
+let magicians = ['Shaheen', 'Naseem', 'Haris'];
+
+function show_magicians(magicians) {
+    for (let i = 0; i < magicians.length; i++) {
+        console.log(magicians[i]);
+    }
+
+}
+
+show_magicians(magicians);
