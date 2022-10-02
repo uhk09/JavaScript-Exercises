4a7829169ea4e455b7b003d6175535478e3d47d6
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-03 00:49:16 +0500

    Update index.js

--- Checking Usernames/index.js
+++ Checking Usernames/index.js
@@ -2,8 +2,9 @@ let current_users = ['taha', 'kamran', 'faisal', 'khurram', 'ali'];
 let new_users = ['shahzad', 'saleem', 'ali', 'faisal', 'haseeb'];
 
 for (let i = 0; i < current_users.length; i++) {
-    for (let j = 0; j < current_users.length; j++) {
-        if () {
+    for (let j = 0; j < new_users.length; j++) {
+        if (current_users[i] === new_users[j]) {
+            console.log(new_users[j] + ' please choose another user name.');
 
         }
 
