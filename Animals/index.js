2e1f23fc05e395837f8dbd26c6a908726f9d0d6c
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-02 12:29:32 +0500

    Create index.js

---
+++ Animals/index.js
@@ -0,0 +1,16 @@
+let animals = ['cow', 'goat', 'lamb'];
+
+for (let i = 0; i < animals.length; i++) {
+
+    if (i === 0) {
+        console.log(animals[i] + ' has 7 share in qurbani.');
+
+    } else if (i === 1) {
+        console.log(animals[i] + ' has 1 share in qurbani and normally expensive than all other animals');
+
+    } else if (i === 2) {
+        console.log(animals[i] + ' has 1 share in qurbani and normally cheaper than goat');
+
+    }
+
+}
