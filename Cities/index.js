a0b28aa3d9133ef1c35bda2ff4d17004c0565565
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-02 13:17:31 +0500

    Create index.js

---
+++ Cities/index.js
@@ -0,0 +1,14 @@
+let city1 = 'Karachi';
+let city2 = 'Lahore';
+let city3 = "London";
+let countryName = 'Pakistan';
+let countryName1 = 'United Kingdom';
+
+function describe_city(cityName, countryName) {
+
+    console.log(cityName + ' is in the ' + countryName)
+}
+
+describe_city(city1, countryName);
+describe_city(city2, countryName);
+describe_city(city3, countryName1);
