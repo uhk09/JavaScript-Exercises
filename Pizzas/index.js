3772cd86d3e6ed77046385e3c97b650acfeea723
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-02 12:21:11 +0500

    Create index.js

---
+++ Pizzas/index.js
@@ -0,0 +1,7 @@
+let pizzaType = ['Chicken Creamy', 'Crispy Crust', 'Chicken Supreme'];
+
+for (let i = 0; i < 3; i++) {
+    console.log(pizzaType[i] + ' is one of the deliciuos one available in the market.');
+}
+
+console.log('I really like pizza so much.' + '\nI like it due the flavour and ingredients use in it' + '\nThe more loaded pizza the more I like it.' + '\nI really love pizza!');
