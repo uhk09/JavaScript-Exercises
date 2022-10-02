d0962edeac17ca380759b947194c39392f42bb9f
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2022-10-02 12:38:50 +0500

    Create index.js

---
+++ T-Shirt/index.js
@@ -0,0 +1,11 @@
+let sizeShirt = 16
+let messageShirt = '"I love coding"';
+
+function make_shirt(sizeShirt, messageShirt) {
+
+
+    console.log(sizeShirt + ' is the size of shirt' + '\nWhile the required message is ' + messageShirt);
+
+}
+
+make_shirt(sizeShirt, messageShirt);
